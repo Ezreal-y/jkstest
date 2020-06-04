4803abc6dc6e76f25e240c1d5c895363b1a9d36c
Ezreal-y <1027832188@qq.com>
2020-06-04 19:27:08 +0800

    创建项目

--- src/main/java/com/jenkins/jkstest/JkstestApplication.java
+++ src/main/java/com/jenkins/jkstest/JkstestApplication.java
@@ -2,10 +2,10 @@ package com.jenkins.jkstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class JkstestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JkstestApplication.class, args);
     }
