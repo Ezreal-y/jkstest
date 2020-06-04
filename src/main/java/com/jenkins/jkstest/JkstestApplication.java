8468b095f280b3cf22eedf4f3ce1c304565a332c
Ezreal-y <1027832188@qq.com>
2020-06-04 17:25:44 +0800

    创建项目

---
+++ src/main/java/com/jenkins/jkstest/JkstestApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jkstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JkstestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/jkstest/JkstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jkstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
