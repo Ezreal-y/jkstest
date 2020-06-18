e652b7bacc77620664d1e0e1e2bd003af85405f2
Ezreal-y <1027832188@qq.com>
2020-06-18 16:07:41 +0800

    jks脚本提交

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@
 #BUILD_ID=DONTKILLME #后台执行
 
 #查询jar包pid
-pid=$(ps -ef|grep jkstest-0.0.1-SNAPSHOT.jar|grep -v grep | awk '{print $2}')
+jks_test_pid=$(ps -ef|grep jkstest-0.0.1-SNAPSHOT.jar|grep -v grep | awk '{print $2}')
 
 #copy jar 到启动目录
 cp -r /var/lib/jenkins/workspace/d/target/jkstest-0.0.1-SNAPSHOT.jar  /home/soul/lgy/jksjar
 
 # 关闭已经启动的jar进程 shell中利用 -n 来判定字符串非空
 function stop(){
-if [ -n "$pid" ]
+if [ -n "$jks_test_pid" ]
   then
   	echo "pid进程 :$pid"
-  	kill -9 $pid
+  	kill -9 $jks_test_pid
  else
     echo "进程没有启动"
 fi
